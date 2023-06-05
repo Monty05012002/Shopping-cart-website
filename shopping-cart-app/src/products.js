4924023ac78096448004f3cd43677d9cc759f17e
Ankit <ankit05mohanty.jsr@gmail.com>
2023-06-05 23:08:40 +0530

    first commit

---
+++ shopping-cart-app/src/products.js
@@ -0,0 +1,66 @@
+import product1 from "./assets/iphone.jpg";
+import product2 from "./assets/macbook.jpeg";
+import product3 from "./assets/camera.jpg";
+import product4 from "./assets/jacket.jpeg";
+import product5 from "./assets/led.jpg";
+import product6 from "./assets/ls.jpeg";
+import product7 from "./assets/t-shirt.jpeg";
+import product8 from "./assets/hat.jpeg";
+import product9 from "./assets/pant.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 50000.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2022 (M1)",
+    price: 120000.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Cannon M50 Camera",
+    price: 35000.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "WLS Van Gogh Denim Jacket",
+    price: 8000.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "LED Light Strips",
+    price: 890.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "SPECTRUM LS TEE",
+    price: 38900.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "AUTO SERVICE SHIRT by GOLF WANG",
+    price: 1200.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "DON'T TRIP UNSTRUCTURED HAT",
+    price: 400.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "PANT",
+    price: 400.0,
+    productImage: product9,
+  },
+];
